78b30fc46ed580ba8e05f43944f4d38b35f28b67
Каушлы Сергей <Kaushly@yandex.ru>
2020-06-30 16:32:53 +0300

    HW six lesson

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat("Vasay", 10, 200, 0);
         Dog dog = new Dog("tod", 4, 500, 200);
-        Dog dog2 = new Dog("tod1", 4, 500, 200);
+        Dog dog2 = new Dog("tod1", 5, 500, 200);
         Dog dog3 = new Dog("tod2", 4, 500, 200);
         cat.run(2000);
         cat.swim(200);
